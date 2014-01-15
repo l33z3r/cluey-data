2dccd4c61d1f900c828280df5cc7842c9f060da2
Lee Farrell <lee.farrell86@gmail.com>
2014-01-15 21:55:25 +0000

    failing test serialize()

--- tests/unit/model.js
+++ tests/unit/model.js
@@ -32,6 +32,24 @@ test("can filter by hash", function() {
   equal(App.Person.first({ name: 'Gavin' }), gavin);
 });
 
+var defaultValPerson;
+
+module('test attr with default val', {
+	setup: function() {
+		defaultValPerson = App.Person.create()
+	},
+	teardown: function() {
+		CD.clear()
+	}
+});
+
+test("test default value serialization", function() {
+	console.log("here ", defaultValPerson);
+	defaultValPerson.serialize();
+	console.log("here")
+	equal(defaultValPerson.serialize().age, -1)
+});
+
 var sully;
 
 module('delete()', {
