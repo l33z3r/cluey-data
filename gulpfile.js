937a8563fa8310b7547d734fff66c73b2b4c085f
Gavin Joyce <gavinjoyce@gmail.com>
2014-01-15 11:53:35 +0000

    uglifying the production output

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function() {
   gulp.src(['src/main.js'])
       .pipe(browserify())
       .pipe(concat('cluey-data.js'))
+      .pipe(uglify())
       .pipe(gulp.dest('build'));
 })
 
