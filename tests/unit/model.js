874f3f7b179c83084a0362e67a5903a7f5fd64a3
Gavin Joyce <gavinjoyce@gmail.com>
2014-01-15 14:38:29 +0000

    added additional test

--- tests/unit/model.js
+++ tests/unit/model.js
@@ -1,11 +1,13 @@
 module("model.js");
 
 test("can filter by hash", function() {
-  App.Person.create({ name: "Lee" });
-  App.Person.create({ name: "Gavin" });
-  App.Person.create({ name: "Zoltan" });
+  App.Person.create({ name: "Lee", age: 10 });
+  App.Person.create({ name: "Gavin", age: 11 });
+  App.Person.create({ name: "Zoltan", age: 12 });
 
-  var filtered = App.Person.find({ name: "Zoltan" });
-
+  var filtered = App.Person.find({ name: "Zoltan", age: 12 });
   equal(filtered.length, 1);
+
+  filtered = App.Person.find({ name: "Zoltan", age: 14 });
+  equal(filtered.length, 0);
 });
